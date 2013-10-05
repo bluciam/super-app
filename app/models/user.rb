4bfeafa63182563374173a3c531875b3dce45cb5
Blanca Mancilla @ ursus <blancalmancilla@gmail.com>
2013-10-05 11:52:21 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   # A validation formula for the attribute name
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   # A validation formula for the attribute email, including following a format
   # and inforcing uniques (translated into an index in the database
   validates :email, presence: true,
