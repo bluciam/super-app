76bcf4aac3976a92fc7f06c82f440652eb382aac
Blanca Mancilla @ ursus <blancalmancilla@gmail.com>
2013-10-19 11:54:54 -0400

    Finish user edit, update, index and destroy actions. Now Improvements.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    name      "My Lovecito"
-    email     "lovecito@wonderworld.com"
-    password  "foobar"
+#    name      "My Lovecito"
+#    email     "lovecito@wonderworld.com"
+    sequence(:name)    { |n| "Lovecito #{n}" }
+    sequence(:email)   { |n| "Lovecito_#{n}@example.com" }
+    password "foobar"
     password_confirmation  "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
